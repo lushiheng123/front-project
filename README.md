0ffdffc00fe6e431f444ad9d2aa7c9c81e634e5b
unknown <robertpierce2828@gmail.com>
2025-03-16 23:40:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 这个页面不错
+
+```sh
+git init
+git add .
+git commit -m "first commit"
+git checkout -b Page1
+git remote add origin git@github.com:lushiheng123/front-project.git
+git push -u origin Page1
+```
+
+![alt text](README_Images/README/image.png)
+![alt text](README_Images/README/image-1.png)
+![alt text](README_Images/README/image-2.png)
---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,35 @@
+import React,{useState,useEffect} from 'react'
+import DarkPng from '../../assets/website/dark-mode-button.png'
+import LightPng from "../../assets/website/light-mode-button.png"
+const DarkMode = () => {
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light")
+    // const element = document.documentElement;
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark")
+            localStorage.setItem("theme", "dark");
+        }
+        else {
+            document.documentElement.classList.remove("dark")
+            localStorage.setItem("theme", "light");
+        }
+    },[theme]);
+    return(
+        <>
+             <div className='relative'>
+                <img src={LightPng} alt="dark" className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10
+                ${theme==="dark"?"opacity-0":"opacity-100"}
+                `}
+                    onClick={() => setTheme(theme === "dark" ? "light" : "dark")} />
+                
+
+                 <img src={DarkPng} alt="dark" className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] duration-300 
+               "
+                onClick={()=>setTheme(theme==="dark"?"light":"dark")} />
+            </div>
+
+            
+        </>
+)
+};
+export default DarkMode
